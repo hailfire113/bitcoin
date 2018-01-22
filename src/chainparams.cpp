61ddd96689d9c9e71a725e248d3d5b61a49eec81
alanra57 <hailfire113@users.noreply.github.com>
2018-01-22 05:08:22 -0500

    Final Fix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -102,10 +102,10 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1510704000; // November 15th, 2017.
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000723d3581fe1bd55373540a");
+        consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000001600160016");
 
         // By default assume that the signatures in ancestors of this block are valid.
-        consensus.defaultAssumeValid = uint256S("0x0000000000000000003b9ce759c2a087d52abc4266f8f4ebd6d768b89defa50a"); //477890
+        consensus.defaultAssumeValid = uint256S("0x000000005600bb86500e725e9e1e6d93570e7e0da96b5ea477d096bdf7a6f7c9"); //477890
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
